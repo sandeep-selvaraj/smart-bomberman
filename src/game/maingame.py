1d7291adee0234ba7d95276b3c4f1344daa7a959
Sandeep <Sandeep@LAPTOP-MQ86F5S0>
2023-04-29 10:45:44 +0200

    feat(maingame): handle reading map from csv

--- src/game/maingame.py
+++ src/game/maingame.py
@@ -2,6 +2,7 @@
 import sys
 from pathlib import Path
 from typing import List
+import csv
 import pygame
 from . import settings, level
 
@@ -9,7 +10,6 @@ _SCREEN = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT
 _CURRENT_PATH = Path.cwd()
 _MAP_FOLDER = "maps"
 _MAP_PATH = _CURRENT_PATH.joinpath(_MAP_FOLDER)
-# test_tile = pygame.sprite.Group(tile.Tile((100, 100), 100))
 
 
 def start_game(level_number: int):
@@ -24,16 +24,16 @@ def start_game(level_number: int):
                 sys.exit()
         _SCREEN.fill("black")
         level_map.run()
-        # test_tile.draw(_SCREEN)
 
         pygame.display.update()
 
 
 def _get_all_levels() -> List:
-    """Read level data from map."""
+    """Read  and store different maps."""
     available_level_files = list(_MAP_PATH.iterdir())
-    all_levels_map = []
+    all_maps = []
     for level_file in available_level_files:
-        with open(level_file, encoding="utf-8") as file:
-            all_levels_map.append(file.readlines())
-    return all_levels_map
+        with open(level_file, newline='', encoding="utf-8-sig") as file:
+            reader = csv.reader(file)
+            all_maps.append(list(reader))
+    return all_maps
