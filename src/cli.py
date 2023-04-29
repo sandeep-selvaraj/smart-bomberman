81004d216d936da34e634b787e85e69db22cdccb
Sandeep <Sandeep@LAPTOP-MQ86F5S0>
2023-04-29 10:44:30 +0200

    refactor(cli): fix mypy issue

--- src/cli.py
+++ src/cli.py
@@ -1,11 +1,13 @@
 """Command Line Interface for running the game"""
-import fire
+import fire  # type: ignore
 
 from game.maingame import start_game
+
 
 def run_the_game():
     """Main function to run the game"""
     start_game(1)
+
 
 if __name__ == "__main__":
     fire.Fire(run_the_game)
