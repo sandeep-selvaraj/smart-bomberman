6466834ce78306ab0b25f43f6e38172cdbe46d59
Sandeep <Sandeep@LAPTOP-MQ86F5S0>
2023-05-04 09:50:56 +0200

    refactor(settings): add enums as per PR suggestions

--- src/game/settings.py
+++ src/game/settings.py
@@ -1,4 +1,16 @@
 """File to deal with game window settings."""
-TILE_SIZE = 34
-SCREEN_WIDTH = 24*TILE_SIZE
-SCREEN_HEIGHT = 14*TILE_SIZE
+from enum import Enum
+
+
+class Game(Enum):
+    """
+    Enum basically to deal with Game definitions.
+    """
+    TILE_SIZE = 34
+
+class GameWindow(Enum):
+    """
+    Enum to deal with the Game Window attributes.
+    """
+    SCREEN_WIDTH = 24*Game.TILE_SIZE.value
+    SCREEN_HEIGHT = 14*Game.TILE_SIZE.value
