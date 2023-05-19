aa5c814daf3896b77bbbe5eb5b980bb62f8a62fa
Sandeep Selvaraj <selvaraj.sandeep@yahoo.co.in>
2023-05-19 17:18:56 +0200

    fix(cli): resolve path issue

--- src/cli.py
+++ src/cli.py
@@ -3,10 +3,13 @@ import fire  # type: ignore
 
 # from game.maingame import start_game
 from game.menu import main_menu
+from pathlib import Path
+import os
 
 
 def run_the_game(level: int = 1):
     """Main function to run the game"""
+    os.chdir(Path(__file__).resolve().parent)
     main_menu(level)
     # start_game(level)
 
