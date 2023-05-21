79a6eac1bf32b1e8cbf61128590b78a8aaea4833
Divyendu Dutta <connect2divyendu@gmail.com>
2023-05-21 18:35:04 +0200

    feat(player): major changes to handle player bomb mechanic

--- src/game/player.py
+++ src/game/player.py
@@ -1,9 +1,11 @@
 """Setting up the player character, interaction"""
 
-from typing import Tuple
+from typing import Tuple, List
 import pygame
 from .constants import PlayerBomberman, PlayerStatus
 from .utils.fileutils import import_from_spritesheet
+from . import bomb
+from .settings import Game
 
 class Player(pygame.sprite.Sprite):
     """
@@ -12,12 +14,14 @@ class Player(pygame.sprite.Sprite):
     """
     # pylint: disable=too-many-instance-attributes
 
-    def __init__(self, position: Tuple):
+    def __init__(self, position: Tuple, walls: pygame.sprite.Group):
         """ 
         Parameters
         ----------
         position: Tuple
             initial position of the player on map
+        walls: pygame.sprite.Group
+            group representing all the wall sprites in level
         """
         # pylint: disable=no-member
         # pylint: disable=c-extension-no-member
@@ -30,10 +34,20 @@ class Player(pygame.sprite.Sprite):
         self.image = self.animations[PlayerStatus.IDLE][self.frame_index]
 
         #reduce the size of the bounding box of player for easier player manueverability
-        self.rect = self.image.get_rect(topleft = position).inflate(-2, -2)
+        self.rect = self.image.get_rect(topleft = position).inflate(-2,-2)
         self.direction = pygame.math.Vector2(0,0)
         self.speed = PlayerBomberman.SPEED.value
         self.status = PlayerStatus.IDLE
+        self.bombs: List = []
+        self.bomb_range = PlayerBomberman.BOMB_RANGE.value
+        self.bomb_limit = PlayerBomberman.BOMB_LIMIT.value
+        self.walls = walls
+
+        # to ensure that holding down bomb deploy button doesnt spam bombs
+        self.bomb_deploy_key_pressed = False
+
+        self.level_shifted = [0,0]
+
 
     def build_player_animation_spritesheet(self):
         """creates an internal dictionary of player state animations"""
@@ -72,8 +86,18 @@ class Player(pygame.sprite.Sprite):
             self.direction = pygame.math.Vector2(0,-1)
         elif keys[pygame.K_DOWN]:
             self.direction = pygame.math.Vector2(0,1)
-        else:
+        elif ( not keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]
+               and not keys[pygame.K_UP] and not keys[pygame.K_DOWN]):
+            # nested if here to ensure player stops to place the bomb, cant place bomb when moving
             self.direction = pygame.math.Vector2(0,0)
+            if ( keys[pygame.K_x] and len(self.bombs) < self.bomb_limit
+                                  and not self.bomb_deploy_key_pressed ):
+                self.bomb_deploy_key_pressed = True
+                temp_bomb = pygame.sprite.GroupSingle()
+                temp_bomb.add(self.deploy_bomb())
+                self.bombs.append(temp_bomb)
+            elif not keys[pygame.K_x]:
+                self.bomb_deploy_key_pressed = False
 
     def player_status(self):
         """sets the player status based on movement"""
@@ -82,6 +106,60 @@ class Player(pygame.sprite.Sprite):
         else:
             self.status = PlayerStatus.IDLE
 
+    def deploy_bomb(self) -> pygame.sprite.Sprite:
+        """places the bomb on level"""
+        bomb_deploy_pos = self._get_grid_aligned_bomb_position([self.rect.x, self.rect.y])
+        return bomb.Bomb(bomb_deploy_pos, self.bomb_range, self.walls)
+
+    def _clean_up_bombs_after_explosion(self):
+        """remove bombs which have been exploded from players internal list"""
+        for temp_bomb in self.bombs.copy():
+            if temp_bomb.sprite.has_explosion_ended:
+                self.bombs.remove(temp_bomb)
+
+    def _get_grid_aligned_bomb_position(self, position):
+        """grid align the bombs based on level shift and offset"""
+        # pylint: disable=too-many-branches
+        # calculate level offset based on overall level shift
+        level_offset = [0,0]
+        if (abs(self.level_shifted[0])%Game.TILE_SIZE.value) != 0:
+            level_offset[0] = Game.TILE_SIZE.value-(abs(self.level_shifted[0])%Game.TILE_SIZE.value)
+        if (abs(self.level_shifted[1])%Game.TILE_SIZE.value) != 0:
+            level_offset[1] = Game.TILE_SIZE.value-(abs(self.level_shifted[1])%Game.TILE_SIZE.value)
+        # add offset to current bomb position
+        if self.level_shifted[0] < 0:
+            position[0] -= level_offset[0]
+        elif self.level_shifted[0] > 0:
+            position[0] += level_offset[0]
+        if self.level_shifted[1] < 0:
+            position[1] -= level_offset[1]
+        elif self.level_shifted[0] > 0:
+            position[1] += level_offset[1]
+        # grid alignment code below - based on tile size
+        grid_aligned_pos = position
+        if position[0] % Game.TILE_SIZE.value != 0:
+            if position[0] % Game.TILE_SIZE.value < (Game.TILE_SIZE.value // 2):
+                grid_aligned_pos[0] = position[0] - (position[0]%Game.TILE_SIZE.value)
+            else:
+                grid_aligned_pos[0] = ( Game.TILE_SIZE.value +
+                                      ((position[0] // Game.TILE_SIZE.value)*Game.TILE_SIZE.value))
+        if position[1] % Game.TILE_SIZE.value != 0:
+            if position[1] % Game.TILE_SIZE.value < (Game.TILE_SIZE.value // 2):
+                grid_aligned_pos[1] = position[1] - (position[1]%Game.TILE_SIZE.value)
+            else:
+                grid_aligned_pos[1] = ( Game.TILE_SIZE.value +
+                                      ((position[1] // Game.TILE_SIZE.value)*Game.TILE_SIZE.value))
+        # add the level offset back after grid alignment
+        if self.level_shifted[0] < 0:
+            grid_aligned_pos[0] += level_offset[0]
+        elif self.level_shifted[0] > 0:
+            grid_aligned_pos[0] -= level_offset[0]
+        if self.level_shifted[1] < 0:
+            grid_aligned_pos[1] += level_offset[1]
+        elif self.level_shifted[0] > 0:
+            grid_aligned_pos[1] -= level_offset[1]
+        return grid_aligned_pos
+
     def update(self):
         """
         updates player state like position based on input
@@ -89,3 +167,4 @@ class Player(pygame.sprite.Sprite):
         self.player_input()
         self.player_status()
         self.animate()
+        self._clean_up_bombs_after_explosion()
